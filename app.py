8691c11cfd5670ccde16b08f5643fe7dfb73e994
Mayuri Jain <pjian@salientprocess.com>
2020-12-27 22:39:48 +0530

    requirement

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, request, jsonify
 from flask.logging import create_logger
 import logging
 
-#import pandas as pd
-#from sklearn.externals import joblib
-#from sklearn.preprocessing import StandardScaler
+import pandas as pd
+from sklearn.externals import joblib
+from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 LOG = create_logger(app)
@@ -14,9 +14,9 @@ def scale(payload):
     """Scales Payload"""
 
     LOG.info("Scaling Payload: %s payload")
-    #scaler = StandardScaler().fit(payload)
-    #scaled_adhoc_predict = scaler.transform(payload)
-    #return scaled_adhoc_predict
+    scaler = StandardScaler().fit(payload)
+    scaled_adhoc_predict = scaler.transform(payload)
+    return scaled_adhoc_predict
 
 @app.route("/")
 def home():
@@ -55,19 +55,18 @@ def predict():
     """
 
     try:
-        #clf = joblib.load("boston_housing_prediction.joblib")
-         LOG.info("JSON payload: TRY json_payload")
+        clf = joblib.load("boston_housing_prediction.joblib")
     except:
         LOG.info("JSON payload: %s json_payload")
         return "Model not loaded"
 
     json_payload = request.json
     LOG.info("JSON payload: %s json_payload")
-    #inference_payload = pd.DataFrame(json_payload)
+    inference_payload = pd.DataFrame(json_payload)
     LOG.info("inference payload DataFrame: %s inference_payload")
-    #scaled_payload = scale(inference_payload)
-    #prediction = list(clf.predict(scaled_payload))
-    return "Sab Badiya he" #jsonify({'prediction': prediction})
+    scaled_payload = scale(inference_payload)
+    prediction = list(clf.predict(scaled_payload))
+    return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
